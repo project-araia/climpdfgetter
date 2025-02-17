17ce0ae1f1020f588fe86270d2bfa7f4857a2921
jlnav <jnavarro@anl.gov>
2025-02-17 15:45:47 -0600

    detect input text encoding with chardet, additional steps to clean the text including avoiding a subsection if urls make up more than a third of it, attempting to remove any remaining urls from okay subsections, removing phone numbers, and removing groups of sequential non-alphanumeric characters. fix bug preventing last subsection from being added to document output json

--- src/climpdfgetter/convert.py
+++ src/climpdfgetter/convert.py
@@ -5,8 +5,12 @@ import re
 import click
 from tqdm import tqdm
 import json
+import chardet
 
 from .schema import ParsedDocumentSchema
+
+# regex from https://www.geeksforgeeks.org/python-check-url-string/ - cant answer any questions about it :)
+URL_RE = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
 
 
 def _prep_path(item: Path):
@@ -103,8 +107,41 @@ def convert(source: Path):
     click.echo("* Failures: " + str(fail_count))
 
 
-def _doesnt_contain_url(text: str):
-    return not re.match(".*http[s]?://", text) and not re.match(".*www.?", text)
+def _is_url_dominant(text: str):
+    """if more than a third of the characters in the subsection belong to URLs, return True"""
+    all_urls = re.findall(URL_RE, text)
+    all_urls_chars = "".join([i[0] for i in all_urls])
+    if len(all_urls_chars) > len(text) / 3:
+        return True
+    return False
+
+
+def _strip_urls(text: str):
+    """remove URLs from text"""
+    all_urls = re.findall(URL_RE, text)
+    all_urls = [i[0] for i in all_urls]
+    for i in all_urls:
+        text = text.replace(i, "")
+    return text
+
+
+def _strip_phone_numbers(text: str):
+    """remove phone numbers from text"""
+    all_phone_numbers = re.findall(
+        r"(\d{3}[-.]?\d{3}[-.]?\d{4}|\(\d{3}\)\s*\d{3}[-.]?\d{4}|\d{3}[-.]?\d{4})",
+        text,
+    )
+    for i in all_phone_numbers:
+        text = text.replace(i, "")
+    return text
+
+
+def _strip_sequential_nonalphanumeric(text: str):
+    """remove groups of 3+ consecutive non-alphanumeric characters from text"""
+    all_groups = re.findall("[^a-zA-Z0-9]{3,}", text)
+    for i in all_groups:
+        text = text.replace(i, " ")
+    return text
 
 
 @click.command()
@@ -112,7 +149,6 @@ def _doesnt_contain_url(text: str):
 def epaocr2json(source: Path):
     """Convert EPA's OCR fulltext to similar json format as output from pdf2json"""
 
-    from text_processing.pdf_to_text import text2json
     from bs4 import BeautifulSoup
 
     collected_input_files = _collect_from_path(Path(source))
@@ -135,7 +171,10 @@ def epaocr2json(source: Path):
         try:
 
             with open(i, "rb") as f:
-                full_text = f.read().decode(errors='ignore')
+                data = f.read()
+
+            encoding = chardet.detect(data)["encoding"]
+            full_text = data.decode(encoding)
 
             pubnumber = re.findall("<pubnumber>(.*?)</pubnumber>", full_text)[0]
             title = re.findall("<title>(.*?)</title>", full_text)[0]
@@ -155,7 +194,10 @@ def epaocr2json(source: Path):
 
             for section in sub_sections:
                 cleaned = "".join([i.strip() for i in section.split("\n") if len(i)])
-                if len(cleaned) and _doesnt_contain_url(cleaned):
+                if len(cleaned) and not _is_url_dominant(cleaned):
+                    cleaned = _strip_urls(cleaned)
+                    cleaned = _strip_phone_numbers(cleaned)
+                    cleaned = _strip_sequential_nonalphanumeric(cleaned)
                     if not cleaned.endswith(" "):
                         cleaned_subsections.append(cleaned)
                     else:  # want to combine lines that are continuations
@@ -164,6 +206,10 @@ def epaocr2json(source: Path):
                     if not cached_section.endswith(" ") and len(cached_section):
                         cleaned_subsections.append(cached_section)
                         cached_section = ""
+
+            cleaned_subsections.append(
+                cleaned
+            )  # append last section, since it doesn't have a continuation
 
             # remove pubnumber from first section
             cleaned_subsections[0] = cleaned_subsections[0].replace(pubnumber, "")
